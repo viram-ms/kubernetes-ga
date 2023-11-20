1e458dd2b8fa2c7700126cb79b0246831c8ce1c7
viram shah <viram1999@gmail.com>
2023-11-20 15:14:44 +0530

    updated files

--- .github/scripts/fetch-pr-title.sh
+++ .github/scripts/fetch-pr-title.sh
@@ -1,50 +1,52 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+  echo "No argument provided. Usage: $0 <your_argument>"
+  exit 1
+fi
+
 # Get the pull request title
-PR_TITLE=$1
-echo $PR_TITLE
+pr_title=$1
+# Convert the pull request title to lowercase
+lowercase_pr_title=$(echo "$input_argument" | tr '[:upper:]' '[:lower:]')
 # Define the environment names
 # ENV_NAMES=("sqa3" "stg" "prod")
-ENV_NAMES=($(ls -1 --color=never environments/fast  | grep -v ":" | grep -v '^[[:space:]]*$'))
+env_names=($(ls -1 --color=never environments/fast  | grep -v ":" | grep -v '^[[:space:]]*$'))
 # SVC_NAMES=("identity" "event" "auditlog" "authentication" "proxy" "email" "saml" "rbac")
-SVC_NAMES=($(ls -1 --color=never charts  | grep -v ":" | grep -v '^[[:space:]]*$'))
+svc_names=($(ls -1 --color=never charts  | grep -v ":" | grep -v '^[[:space:]]*$'))
 # SILO_NAMES=("fast")
-SILO_NAMES=($(ls -1 --color=never environments  | grep -v ":" | grep -v '^[[:space:]]*$'))
-
-
-echo "line 15 $ENV_NAMES"
-echo "line 16 $SVC_NAMES"
-echo "line 17 $SILO_NAMES"
+silo_names=($(ls -1 --color=never environments  | grep -v ":" | grep -v '^[[:space:]]*$'))
 
 
 # Loop through the environment names
-for ENV_NAME in "${ENV_NAMES[@]}"; do
-    echo "lie 22 $ENV_NAME"
+for env_name in "${env_name[@]}"; do
+    echo "lie 23 $env_name"
  # Check if the environment name is in the pull request title
-    if [[ $PR_TITLE == "$ENV_NAME" ]]; then
-        echo "environment=$ENV_NAME" >> "$GITHUB_ENV"
+    if [[ $lowercase_pr_title == *"$env_name"* ]]; then
+        echo "line 26"
+        echo "environment=$env_name" >> "$GITHUB_ENV"
         break
     fi
 done
 
-for SVC_NAME in "${SVC_NAMES[@]}"; do
-    echo "line 31 $SVC_NAME"
+for svc_name in "${svc_names[@]}"; do
+    echo "line 33 $svc_names"
  # Check if the service name is in the pull request title
-    if [[ $PR_TITLE == "$SVC_NAME" ]]; then
-        echo "line 34"
-        echo "service=$SVC_NAME" >> "$GITHUB_ENV"
+    if [[ $lowercase_pr_title == *"$svc_name"* ]]; then
+        echo "line 36"
+        echo "service=$svc_name" >> "$GITHUB_ENV"
         break
     fi
 done
 
-for SILO_NAME in "${SILO_NAMES[@]}"; do
-    echo "line 40 $SILO_NAME"
+for silo_name in "${silo_names[@]}"; do
+    echo "line 43 $silo_name"
  # Check if the silo is in the pull request title.
-    if [[ $PR_TITLE == "$SILO_NAME" ]]; then
-        echo "line 44"
-        echo "silo=$SILO_NAME" >> "$GITHUB_ENV"
+    if [[ $lowercase_pr_title == *"$silo_name"* ]]; then
+        echo "line 46"
+        echo "silo=$silo_name" >> "$GITHUB_ENV"
         break
     fi
 done
 
-echo "line 50 $service $environment $silo"
+echo "line 52 $service $environment $silo"
