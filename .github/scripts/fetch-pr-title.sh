ab4bc35850068e958fa3e9a51a9e7330b097743c
viram shah <viram1999@gmail.com>
2023-11-20 12:19:30 +0530

    github actions

---
+++ .github/scripts/fetch-pr-title.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Get the pull request title
+PR_TITLE="${GITHUB_EVENT_NAME}"
+
+# Define the environment names
+# ENV_NAMES=("sqa3" "stg" "prod")
+ENV_NAMES=($(ls -1 --color=never ../../environments/fast  | grep -v ":" | grep -v '^[[:space:]]*$'))
+# SVC_NAMES=("identity" "event" "auditlog" "authentication" "proxy" "email" "saml" "rbac")
+SVC_NAMES=($(ls -1 --color=never ../../charts  | grep -v ":" | grep -v '^[[:space:]]*$'))
+# SILO_NAMES=("fast")
+SILO_NAMES=($(ls -1 --color=never ../../environments  | grep -v ":" | grep -v '^[[:space:]]*$'))
+
+
+# Loop through the environment names
+for ENV_NAME in "${ENV_NAMES[@]}"; do
+ # Check if the environment name is in the pull request title
+    if [[ $PR_TITLE == "$ENV_NAME" ]]; then
+        echo "environment: $ENV_NAME" >> "$GITHUB_ENV"
+        break
+    fi
+done
+
+for SVC_NAME in "${SVC_NAMES[@]}"; do
+ # Check if the environment name is in the pull request title
+    if [[ $PR_TITLE == "$SVC_NAME" ]]; then
+        echo "service: $SVC_NAME" >> "$GITHUB_ENV"
+        break
+    fi
+done
+
+for SILO_NAME in "${SILO_NAMES[@]}"; do
+ # Check if the environment name is in the pull request title
+    if [[ $PR_TITLE == "$SILO_NAME" ]]; then
+        echo "silo: $SILO_NAME" >> "$GITHUB_ENV"
+        break
+    fi
+done
---
+++ .github/scripts/update-version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Extract the version number from the Chart.yaml file
+version=$(yq e '.version' Chart.yaml)
+
+# Split the version number into major, minor, and patch components
+IFS='.' read -ra ADDR <<< "$version"
+major="${ADDR[0]}"
+minor="${ADDR[1]}"
+patch="${ADDR[2]}"
+
+IFS='-' read -ra ADDDR <<< "$patch"
+patchnumber="${ADDDR[0]}"
+
+# Increment the patch component
+patchnumber=$((patchnumber+1))
+
+# Construct the new version number
+new_version="$major.$minor.$patchnumber"
+
+# Update the version number in the Chart.yaml file
+# yq e ".version = \"$new_version\"" -i Chart.yaml
+echo "{new_version}={$new_version}" >> "$GITHUB_ENV"
