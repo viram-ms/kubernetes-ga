02b2e46574de998054bd64d86c6ae5031c21dc38
viram shah <viram1999@gmail.com>
2023-11-22 12:47:44 +0530

    function position updated

--- .github/scripts/update-version.sh
+++ .github/scripts/update-version.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+update_version() {
+    # Split the version number into major, minor, and patch components
+    IFS='.' read -ra ADDR <<< "$1"
+    major="${ADDR[0]}"
+    minor="${ADDR[1]}"
+    patch="${ADDR[2]}"
+
+    IFS='-' read -ra ADDDR <<< "$patch"
+    patchnumber="${ADDDR[0]}"
+
+    # Increment the patch component
+    patchnumber=$((patchnumber+1))
+
+    # Construct the new version number
+    new_version="$major.$minor.$patchnumber"
+
+    # Update the version number in the Chart.yaml file
+    # yq e ".version = \"$new_version\"" -i Chart.yaml
+    echo $new_version
+    echo "new_version=$new_version" >> "$GITHUB_ENV"
+}
+
 # Extract the version number from the Chart.yaml file
 path=$1
 echo $path
@@ -29,25 +51,5 @@ if [[ $label_found == "false" ]]; then
     echo "new_version=$version" >> "$GITHUB_ENV"
 fi 
 
-update_version() {
-    # Split the version number into major, minor, and patch components
-    IFS='.' read -ra ADDR <<< "$1"
-    major="${ADDR[0]}"
-    minor="${ADDR[1]}"
-    patch="${ADDR[2]}"
 
-    IFS='-' read -ra ADDDR <<< "$patch"
-    patchnumber="${ADDDR[0]}"
-
-    # Increment the patch component
-    patchnumber=$((patchnumber+1))
-
-    # Construct the new version number
-    new_version="$major.$minor.$patchnumber"
-
-    # Update the version number in the Chart.yaml file
-    # yq e ".version = \"$new_version\"" -i Chart.yaml
-    echo $new_version
-    echo "new_version=$new_version" >> "$GITHUB_ENV"
-}
 
