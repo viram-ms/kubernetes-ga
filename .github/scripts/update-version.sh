1dddb02757a9f250779ced23cbed6fad66fb202d
viram shah <viram1999@gmail.com>
2023-11-22 12:34:04 +0530

    sdfsdf

--- .github/scripts/update-version.sh
+++ .github/scripts/update-version.sh
@@ -2,26 +2,49 @@
 
 # Extract the version number from the Chart.yaml file
 path=$1
+pull_request_labels=$2
 echo $path
+echo $pull_request_labels
 version=$(yq e '.version' $path/Chart.yaml)
 
 echo $version
-# Split the version number into major, minor, and patch components
-IFS='.' read -ra ADDR <<< "$version"
-major="${ADDR[0]}"
-minor="${ADDR[1]}"
-patch="${ADDR[2]}"
 
-IFS='-' read -ra ADDDR <<< "$patch"
-patchnumber="${ADDDR[0]}"
+label_names=$(echo $pull_request_labels | jq -r '.[].name')
+IFS=', ' read -ra labels <<< "$label_names"
+label_found=false
+for label in "${labels[@]}"; do
+    echo "Label: $label"
+    if [ "$label" == "bug" ]; then
+        label_found=true
+        update_version()
+        break
+    fi
+done
 
-# Increment the patch component
-patchnumber=$((patchnumber+1))
+if [[ $label_found == "false" ]]; then
+    version=$(yq e '.version' ${{ matrix.files }}/Chart.yaml)
+    echo "new_version=$version" >> "$GITHUB_ENV"
+fi 
 
-# Construct the new version number
-new_version="$major.$minor.$patchnumber"
+update_version() {
+    # Split the version number into major, minor, and patch components
+    IFS='.' read -ra ADDR <<< "$version"
+    major="${ADDR[0]}"
+    minor="${ADDR[1]}"
+    patch="${ADDR[2]}"
 
-# Update the version number in the Chart.yaml file
-# yq e ".version = \"$new_version\"" -i Chart.yaml
-echo $new_version
-echo "new_version=$new_version" >> "$GITHUB_ENV"
+    IFS='-' read -ra ADDDR <<< "$patch"
+    patchnumber="${ADDDR[0]}"
+
+    # Increment the patch component
+    patchnumber=$((patchnumber+1))
+
+    # Construct the new version number
+    new_version="$major.$minor.$patchnumber"
+
+    # Update the version number in the Chart.yaml file
+    # yq e ".version = \"$new_version\"" -i Chart.yaml
+    echo $new_version
+    echo "new_version=$new_version" >> "$GITHUB_ENV"
+}
+
