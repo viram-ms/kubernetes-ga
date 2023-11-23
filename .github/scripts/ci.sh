3d2b6dedf04d17a0a073d5545e948c7eb381dcb4
viram shah <viram1999@gmail.com>
2023-11-23 11:56:11 +0530

    changes

---
+++ .github/scripts/ci.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+update_version() {
+    # Split the version number into major, minor, and patch components
+    version=$1
+    path=$2
+    IFS='.' read -ra ADDR <<< "$version"
+
+    major="${ADDR[0]}"
+    minor="${ADDR[1]}"
+    patch="${ADDR[2]}"
+
+    IFS='-' read -ra ADDDR <<< "$patch"
+    patchnumber="${ADDDR[0]}"
+
+    # Increment the patch component
+    patchnumber=$((patchnumber+1))
+
+    # Construct the new version number
+    new_version="$major.$minor.$patchnumber"
+
+    # Update the version number in the Chart.yaml file
+    # yq e ".version = \"$new_version\"" -i Chart.yaml
+    echo $new_version
+    echo "new_version=$new_version" >> "$GITHUB_ENV"
+    yq e ".version = \"$new_version\"" -i $path/Chart.yaml
+}
+
+count=$1
+path=$2
+bucket_name="service-helm-charts"
+if [[ $count == 1 ]]; then
+    echo "line 3"
+    
+    version=$(yq e '.version' $path/Chart.yaml)
+    file_key="${{ env.service }}-${{ env.version }}.tgz" 
+    if aws s3api list-objects --bucket "$bucket_name" --prefix "$file_key" | grep -q "$file_key"; then
+        echo "File exists in the bucket and cannot be uploaded in helm chart repository"
+        exit 1
+    else
+        echo "File does not exist in the bucket and can be uploaded in helm chart repository"
+    fi
+fi
+
+if [[ ${{ steps.changed-file-chart-yaml.outputs.all_changed_files_count }} == 0 ]]; then
+    echo "line 17"
+    version=$(yq e '.version' $path/Chart.yaml)
+    update_version $version $path
+    new_version=$(yq e '.version' $path/Chart.yaml)
+    file_key="${{ env.service }}-$new_version.tgz" 
+    if aws s3api list-objects --bucket "$bucket_name" --prefix "$file_key" | grep -q "$file_key"; then
+        echo "File exists in the bucket and cannot be uploaded in helm chart repository"
+        exit 1
+    else
+        echo "File does not exist in the bucket and can be uploaded in helm chart repository"
+    fi
+fi
